436c1d55ac8e75bc27832675c6285e57d0f1a27b
binhtd <binhtd.com@gmail.com>
2019-10-01 22:25:45 +0700

    Test Router

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import { Layout, Menu, Icon, DatePicker } from 'antd';
 import Logo from '../assets/logo.png';
+import Link from 'umi/link';
+
 const { Header, Content, Sider } = Layout;
 const { SubMenu } = Menu;
 
@@ -32,26 +34,26 @@ class BasicLayout extends React.Component {
     return (
       <Layout style={{ minHeight: '100vh' }}>
         <Header style={{ padding: 0, backgroundColor: '#000' }} >
-          <a href="#" style={{marginLeft:'25px', float:'left'}}>
-                <img src={Logo} alt="" style={{ width: '150px' }} />
+          <a href="#" style={{ marginLeft: '25px', float: 'left' }}>
+            <img src={Logo} alt="" style={{ width: '150px' }} />
           </a>
-          <div style={{ display: 'flex', justifyContent:'space-between' }} >
-            <ul style={{ color: '#fff', listStyle: 'none', display:'flex' }}>
-              <li>
-                  <a href="#" style={{border:'1px solid #fff', color:'#000', backgroundColor:'#fff', padding:5}}>
-                    <Icon type="google-plus" style={{paddingRight:2}}/>
-                    <span style={{fontWeight:700}}>Map</span>
-                  </a>
+          <div style={{ display: 'flex', justifyContent: 'space-between' }} >
+            <ul style={{ color: '#fff', listStyle: 'none', display: 'flex' }}>
+              <li style={{ paddingRight: 5 }}>
+                <a href="#" style={{ border: '1px solid #fff', color: '#000', backgroundColor: '#fff', padding: '5px 10px', borderRadius: 4 }}>
+                  <Icon type="google-plus" style={{ paddingRight: 2 }} />
+                  <span style={{ fontWeight: 700 }}>Map</span>
+                </a>
               </li>
               <li>
-                  <DatePicker onChange={this.onChangeDate} placeholder="Today" style={{width:'50%', borderRadius:'0'}}/>
+                <DatePicker onChange={this.onChangeDate} placeholder="Today" style={{ width: 80, borderRadius: '0' }} />
               </li>
             </ul>
-            <ul style={{ color: '#fff', listStyle: 'none', display:'flex' }}>
+            <ul style={{ color: '#fff', listStyle: 'none', display: 'flex', marginRight: 10 }}>
               <li>
-                  <a href="#" style={{color:'#fff', fontSize:'35px'}}>
-                    <Icon type="bell" style={{}}/>
-                  </a>
+                <a href="#" style={{ color: '#fff', fontSize: '35px' }}>
+                  <Icon type="bell" style={{}} />
+                </a>
               </li>
             </ul>
           </div>
@@ -59,47 +61,52 @@ class BasicLayout extends React.Component {
         <Layout>
           <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
             <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-              <Menu.Item key="1">
-                <Icon type="pie-chart" />
-                <span>Option 1</span>
+              <Menu.Item style={{ marginTop: '0' }} key="1">
+                <Link to="/">
+                  <Icon type="pie-chart" />
+                  <span>Tracking</span>
+                </Link>
               </Menu.Item>
               <Menu.Item key="2">
-                <Icon type="desktop" />
-                <span>Option 2</span>
+                <Link to="/report">
+                  <Icon type="desktop" />
+                  <span>Report</span>
+                </Link>
               </Menu.Item>
               <SubMenu
                 key="sub1"
                 title={
                   <span>
                     <Icon type="user" />
-                    <span>User</span>
+                    <span>Field service</span>
                   </span>
                 }
               >
-                <Menu.Item key="3">Tom</Menu.Item>
-                <Menu.Item key="4">Bill</Menu.Item>
-                <Menu.Item key="5">Alex</Menu.Item>
+                <Menu.Item key="3">Task</Menu.Item>
+                <Menu.Item key="4">Templates</Menu.Item>
+                <Menu.Item key="5">Form</Menu.Item>
               </SubMenu>
               <SubMenu
                 key="sub2"
                 title={
                   <span>
                     <Icon type="team" />
-                    <span>Team</span>
+                    <span>Fleet management</span>
                   </span>
                 }
               >
-                <Menu.Item key="6">Team 1</Menu.Item>
-                <Menu.Item key="8">Team 2</Menu.Item>
+                <Menu.Item key="6">Vehicles</Menu.Item>
+                <Menu.Item key="7">Maintenance</Menu.Item>
+                <Menu.Item key="8">Drives</Menu.Item>
               </SubMenu>
               <Menu.Item key="9">
                 <Icon type="file" />
-                <span>File</span>
+                <span>Devices and Settings</span>
               </Menu.Item>
             </Menu>
           </Sider>
           <Content style={{ margin: '0' }}>
-            <div style={{ padding: 24, background: '#fff', minHeight: '90vh' }}>{this.props.children}</div>
+            <div style={{ padding: 24, background: '#fff', minHeight: '100vh' }}>{this.props.children}</div>
           </Content>
         </Layout>
       </Layout>
---
+++ src/pages/report.js
@@ -0,0 +1,14 @@
+export default function() {
+    return (
+      <div>
+        <ul>
+          <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
+          <li>
+            <a href="https://umijs.org/guide/getting-started.html">
+            Get Started
+            </a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
